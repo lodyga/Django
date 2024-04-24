03766a613cca7b1bed49a84d50989507fb28c9fc
Lodyga <lodyga@o2.pl>
2024-04-24 23:30:39 +0200

    update and delete

--- codesite/python_problems/urls.py
+++ codesite/python_problems/urls.py
@@ -5,11 +5,16 @@ from . import views
 app_name = "python_problems"
 
 urlpatterns = [
-    path("", views.IndexView.as_view(), name="index"),
-    path("<int:pk>/", views.DetailView.as_view(), name="detail"),
     path("tag/", views.TagIndexView.as_view(), name="tag-index"),
     path("tag/create/", views.TagCreate.as_view(), name="tag-create"),
+    path("tag/<int:pk>/update/", views.TagUpdate.as_view(), name="tag-update"),
+    path("tag/<int:pk>/delete/", views.TagDelete.as_view(), name="tag-delete"),
+
+    path("", views.ProblemIndexView.as_view(), name="index"),
+    path("<int:pk>/", views.ProblemDetailView.as_view(), name="detail"),
     path("create/", views.ProblemCreate.as_view(), name="problem-create"),
+    path("problem/<int:pk>/update/", views.ProblemUpdate.as_view(), name="problem-update"),
+    path("problem/<int:pk>/delete/", views.ProblemDelete.as_view(), name="problem-delete"),
     
     path("conversion/", views.conversion_view, name="conversion"),
 ]
--- codesite/python_problems/views.py
+++ codesite/python_problems/views.py
@@ -1,36 +1,61 @@
 from django.shortcuts import render
-from django.views import generic
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .models import Tag, Problem
 
 
-class IndexView(generic.ListView):
+class ProblemIndexView(ListView):
     model = Problem
 
 
-class DetailView(generic.DetailView):
+class ProblemDetailView(DetailView):
     model = Problem
 
     def get_context_data(self, **kwargs):
-        context = super(DetailView, self).get_context_data(**kwargs)
+        context = super(ProblemDetailView, self).get_context_data(**kwargs)
         problem = self.get_object()
         context["tags"] = problem.tags.values_list("name", flat=True)
         return context
 
 
-class TagIndexView(generic.ListView):
+class TagIndexView(ListView):
     model = Tag
 
 
-class TagCreate(LoginRequiredMixin, generic.edit.CreateView):
+class TagCreate(LoginRequiredMixin, CreateView):
     model = Tag
     fields = "__all__"
     success_url = reverse_lazy('python_problems:tag-index')
 
 
-class ProblemCreate(LoginRequiredMixin, generic.edit.CreateView):
+class TagUpdate(LoginRequiredMixin, UpdateView):
+    model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy('python_problems:tag-index')
+
+
+class TagDelete(LoginRequiredMixin, DeleteView):
+    model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy('python_problems:tag-index')
+
+
+class ProblemCreate(LoginRequiredMixin, CreateView):
+    model = Problem
+    fields = "__all__"
+    success_url = reverse_lazy('python_problems:index')
+
+
+class ProblemUpdate(LoginRequiredMixin, UpdateView):
+    model = Problem
+    fields = "__all__"
+    success_url = reverse_lazy('python_problems:index')
+
+
+class ProblemDelete(LoginRequiredMixin, DeleteView):
     model = Problem
     fields = "__all__"
     success_url = reverse_lazy('python_problems:index')
