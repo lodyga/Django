823b976dbbe95b74637be0fbbe1a4976a2b15303
Lodyga <lodyga@o2.pl>
2024-04-26 22:42:40 +0200

    search bar

--- codesite/python_problems/views.py
+++ codesite/python_problems/views.py
@@ -1,14 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import ListView, DetailView
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.db.models import Q
 
 from .models import Tag, Problem
 
 
 class ProblemIndexView(ListView):
     model = Problem
+
+
+def problem_index_view(request):
+    query = request.GET.get("query", "")
+    problem_list = Problem.objects.all()
+
+    if query:
+        problem_list = problem_list.filter(Q(tags__name__icontains=query) | Q(title__icontains=query)).distinct()
+
+    context = {
+        "problem_list": problem_list,
+        "query": query,
+    }
+    return render(request, "python_problems/problem_list.html", context)
 
 
 class ProblemDetailView(DetailView):
@@ -18,7 +33,22 @@ class ProblemDetailView(DetailView):
         context = super(ProblemDetailView, self).get_context_data(**kwargs)
         problem = self.get_object()
         context["tags"] = problem.tags.values_list("name", flat=True)
+        context["related_problems"] = Problem.objects.filter(
+            tags__in=problem.tags.all()).exclude(pk=problem.pk).distinct()
         return context
+
+
+# same as ProblemDetailView
+def problem_detail_view(request, pk):
+    problem = get_object_or_404(Problem, pk=pk)
+    related_problems = Problem.objects.filter(
+        tags__in=problem.tags.all()).exclude(pk=pk).distinct()
+
+    context = {
+        "problem": problem,
+        "tags": problem.tags.values_list("name", flat=True),
+        "related_problems": related_problems}
+    return render(request, "python_problems/problem_detail.html", context)
 
 
 class TagIndexView(ListView):
