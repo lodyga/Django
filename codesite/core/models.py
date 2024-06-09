f127003bff49692250076938188a2bb4bc4fc61f
Lodyga <lodyga@o2.pl>
2024-06-09 23:19:28 +0200

    Revert "messing with gravatar and branches"
    This reverts commit d8c7199eff90fa31c1cae7ba7e7a00777828034d.

--- codesite/core/models.py
+++ codesite/core/models.py
@@ -1,10 +1,3 @@
 from django.db import models
-from django.contrib.auth.models import User
 
-
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    avatar_url = models.URLField(max_length=200, blank=True, null=True)
-
-    def __str__(self):
-        return self.user.username
+# Create your models here.
--- codesite/core/social_auth_pipeline.py
+++
@@ -1,7 +0,0 @@
-def save_github_avatar(backend, user, response, *args, **kwargs):
-    if backend.name == 'github':
-        avatar_url = response.get('avatar_url')
-        if avatar_url:
-            profile = user.profile
-            profile.avatar_url = avatar_url
-            profile.save()
