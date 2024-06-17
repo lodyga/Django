49498f610d0b20c21659ad0c1867691b6a291fa7
Lodyga <lodyga@o2.pl>
2024-06-18 00:05:17 +0200

    Solution from Problem  separated.

--- codesite/load_db.py
+++ codesite/load_db.py
@@ -7,14 +7,18 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'codesite.settings')
 django.setup()
 
 from django.contrib.contenttypes.models import ContentType
-from python_problems.models import Tag, Problem  # Import your models
+from python_problems.models import Tag, Difficulty, Complexity, Language, Problem, Solution  # Import your models
 
 # Apply migrations
 call_command('migrate')
 
 # Optional: Clear specific application tables to avoid duplicates
 Tag.objects.all().delete()
+Difficulty.objects.all().delete()
+Complexity.objects.all().delete()
+Language.objects.all().delete()
 Problem.objects.all().delete()
+Solution.objects.all().delete()
 
 # Clear the content types to avoid unique constraint issues
 ContentType.objects.all().delete()
