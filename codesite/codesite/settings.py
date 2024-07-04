c60a7b31f8c220221ae98c5e7e3c2e55cff29587
Lodyga <lodyga@o2.pl>
2024-07-04 14:47:20 +0200

    setup to deploy Codesite as Docker image on Render

--- codesite/codesite/settings.py
+++ codesite/codesite/settings.py
@@ -21,8 +21,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     "127.0.0.1",
+    "ukasz.eu.pythonanywhere.com"
     "localhost",  # for local Docker
-    "ukasz.eu.pythonanywhere.com"]
+    "codesite-latest.onrender.com"  # for Docker container on Render
+]
 
 
 # Application definition
